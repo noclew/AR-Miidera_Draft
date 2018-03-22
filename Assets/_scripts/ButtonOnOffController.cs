80ff8771cf2f0e3a3e362b79c0afa850a5cb6049
noclew <noclews@gmail.com>
2018-03-22 18:45:35 -0400

    first commit with v004

---
+++ Assets/_scripts/ButtonOnOffController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOnOffController : MonoBehaviour
+{
+    public GameObject modelGroup;
+    public GameObject[] onModelList;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked" + this.name);
+        OffAll();
+        OnObjs();
+    }
+
+    void OnObjs()
+    {
+        foreach (GameObject obj in onModelList)
+        {
+            obj.SetActive(true);
+            //var rendererComponents = obj.GetComponentsInChildren<Renderer>(true);
+            //var colliderComponents = obj.GetComponentsInChildren<Collider>(true);
+            //var canvasComponents = obj.GetComponentsInChildren<Canvas>(true);
+
+            //// Disable rendering:
+            //foreach (var component in rendererComponents)
+            //    component.enabled = true;
+
+            //// Disable colliders:
+            //foreach (var component in colliderComponents)
+            //    component.enabled = true;
+
+            //// Disable canvas':
+            //foreach (var component in canvasComponents)
+            //    component.enabled = true;
+        }
+
+    }
+
+    void OffAll()
+    {
+        foreach (Transform child in modelGroup.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+
+    }
+}
---
+++ Assets/_scripts/OnOffData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OnOffData", menuName = "ncScript/On Off Func", order = 1)]
+public class OnOffData : ScriptableObject {
+    public GameObject objectGroup;
+    public GameObject[] onList;
+
+}
